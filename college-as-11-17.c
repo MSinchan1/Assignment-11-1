b06459a8eda948b4d387519fe2d3f2ff476ea2ae
Sinchan Mitra <116640089+MSinchan1@users.noreply.github.com>
2023-04-02 08:15:05 +0530

    Add files via upload

---
+++ college-as-11-17.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+//Factorial fact(5)=120    fact(7)=5040
+int Factorial(int);//Declare the secondary function
+            /*  Main Function */
+void main()
+{
+    int num,n;
+    printf("Enter number: ");
+    scanf("%d",&num);//Enter number 5
+    n=Factorial(num); ////store the function in a variable
+    printf("Factorial is: %d",n);//print the variable
+}
+        /* Secondary Function */
+int Factorial(int x)
+{
+    int fact=1,i;
+    for(i=1;i<=x;i++) //1....2....3....4.....5
+    {
+        fact*=i; //1*1.....1*2.....2*3......6*4......24*5
+    }
+    return fact; //returning the result fact=120
+}
